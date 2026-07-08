4f309b3ef30f80fe550e739404de97e8abfcb2be
dhruvinbhagvagar <dhruvinbhagwagar>
2026-07-08 15:56:30 +0530

    all

---
+++ C programming/Homework/Helloword.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+	printf("My name is Dhruvin");
+	printf("\nim an IT Engineer");
+	printf("\nmy email is dhruvinbhagvagar@gmail.com");
+	printf("\nMy hobby is Cricket");
+	printf("\nTata");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	
+	
+	printf("\n *    *******"   );
+	printf("\n *    *"         );
+	printf("\n *    *"         );
+	printf("\n *    *"         );
+	printf("\n ************  " );
+	printf("\n      *     * "  );
+	printf("\n      *     * "  );
+	printf("\n      *     *  " );
+	printf("\n ******     *  " );
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	
+	
+	printf("\n*****************");
+	printf("\n*               *");
+	printf("\n*               *");
+	printf("\n*    DHRUVIN    *");
+	printf("\n*               *");
+	printf("\n*               *");
+	printf("\n*****************");
+	
+}
---
+++ C programming/Homework/datatypes.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main(){
+	char name[20];
+    int age;
+    float marks;
+    
+    printf("enter your name");
+    scanf("%s",&name);
+    printf("name=%s",name);
+    
+    printf("\n enter your age");
+    scanf("%d",&age);
+    printf("your age is=%d",age);
+    
+  
+   printf("\n enter your marks");
+   scanf("%f",&marks);
+   printf("\n your marks are =%f",marks);
+   
+   
+
+
+}
